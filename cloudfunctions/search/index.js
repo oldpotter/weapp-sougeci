5bd10dcec0caec83edb0b41ed216bde17795ef1e
oldpotter <oldpottertom@icloud.com>
2019-03-23 10:06:23 +0800

    1.4.0
    搜索结果可以加载更多

--- cloudfunctions/search/index.js
+++ cloudfunctions/search/index.js
@@ -5,7 +5,10 @@ cloud.init()
 
 // 云函数入口函数
 exports.main = async (event, context) => new Promise((resolve, reject) => {
-	const url = 'http://shenkeling.top:3000/search?keywords=' + encodeURI(event.keywords)
+	const keywords = encodeURI(event.keywords)
+	const limit = event.limit
+	const offset = event.offset
+	const url = `http://shenkeling.top:3000/search?keywords=${keywords}&limit=${limit}&offset=${offset}`  
 	request(url, (err, res, body) => {
 		resolve(body)
 	})
--- miniprogram/pages/search/search.js
+++ miniprogram/pages/search/search.js
@@ -1,8 +1,57 @@
+const limit = 10;
 Page({
   data: {
     songs: [],
-		src: 'https://shenkeling.top/files/1.jpeg'
+		src: 'https://shenkeling.top/files/1.jpeg',
+		offset: 0,
+		tip: '',
+		loadig: false
   },
+
+	onReachBottom(){
+		const _this = this
+		this.setData({
+			loading: true
+		})
+		wx.cloud.callFunction({
+			name: 'search',
+			data: {
+				keywords: _this.data.keyword,
+				limit,
+				offset: _this.data.offset
+			},
+			success(res) {
+				res = JSON.parse(res.result)
+				if (res.result.songCount > 0) {
+					let songs = res.result.songs.map(song => {
+						return {
+							id: song.id,
+							name: song.name,
+							album: song.album.name,
+							artist: song.artists[0].name
+						}
+					})
+					const offset = _this.data.offset + songs.length
+					songs = _this.data.songs.concat(songs)
+					_this.setData({
+						songs,
+						loading: false,
+						offset
+					})
+				} else {
+					_this.setData({
+						loading: false,
+						tip: '无更多数据'
+					})
+				}
+				wx.hideLoading()
+			},
+
+			fail(err) {
+				console.error(err)
+			}
+		})
+	},
 
   doSearch(e) {
     const _this = this
@@ -11,10 +60,16 @@ Page({
       return
     }
     wx.showLoading({})
+		this.setData({
+			offset: 0,
+			keyword
+		})
     wx.cloud.callFunction({
       name: 'search',
       data: {
-        keywords: keyword
+        keywords: keyword,
+				limit,
+				offset: _this.data.offset
       },
       success(res) {
         res = JSON.parse(res.result)
@@ -27,8 +82,10 @@ Page({
               artist: song.artists[0].name
             }
           })
+					const offset = _this.data.offset + songs.length
           _this.setData({
-            songs
+            songs,
+						offset
           })
         } else {
           _this.setData({
